66e007c501f361c73ed4ddf1b739ce34be577551
Spencer Dearman <dearmanspencer@gmail.com>
2026-04-12 16:10:07 -0500

    Initial Commit

---
+++ DailyTasks Watch App/DailyTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTasksApp.swift
+//  DailyTasks Watch App
+//
+//  Created by Spencer Dearman on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyTasks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
